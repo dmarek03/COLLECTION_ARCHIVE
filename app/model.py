789ef83959603c4b8ba45257f8e978cb5716a87e
dmarek03 <domarek@student.agh.edu.pl>
2024-07-28 19:14:56 +0200

    updated model

--- app/model.py
+++ app/model.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from abc import ABC
 
 
+
 @dataclass
 class BaseTable(ABC):
     id_: int | None = None
@@ -11,6 +12,7 @@ class BaseTable(ABC):
 
 @dataclass
 class FoundedItems(BaseTable):
+    description: str | None = None
     image_data: bytes | None  = None
     quantity: int | None = 0
     addition_date: datetime | None = None
@@ -32,7 +34,10 @@ class Locality(BaseTable):
 
 @dataclass
 class Location(BaseTable):
-    coordinates: str | None = None
+    latitude: float | None = None
+    longitude: float | None = None
+    latitude_direction: str | None = None
+    longitude_direction: str | None = None
 
 
 @dataclass
@@ -42,21 +47,43 @@ class Material(BaseTable):
 
 @dataclass
 class Dating(BaseTable):
-    year: int |  None = None
+    year: int |  str = 'indefinite'
 
 
 @dataclass
 class FinalItem(BaseTable):
+    description: str | None = None
     image_data: bytes | None = None
     quantity: int | None = 0
     addition_date: datetime | None = None
     finder_name: str | None = None
     locality_name: str | None = None
     location_name: str | None = None
-    coordinates: str | None = None
+    latitude: float | None = None
+    longitude: float | None = None
+    latitude_direction: str | None = None
+    longitude_direction: str | None = None
     material_name: str | None = None
     epoch_name: str |  None =  None
-    year: int | None = None
+    year: int | str = None
+
+
+    def __repr__(self):
+        return f'''
+            Name: {self.name},
+            Description: {self.description},
+            Image date: {self.image_data},
+            Quantity: {self.quantity=},
+            Date of addition: {self.addition_date},
+            Name of finder: {self.finder_name},
+            Name of locality: {self.locality_name},
+            Name of location: {self.location_name},
+            Coordinates: {self.latitude}°{self.latitude_direction} , {self.longitude}°{self.longitude_direction},
+            Name of material: {self.material_name},
+            Name of the epoch: {self.epoch_name},
+            Year: {self.year}
+
+        '''
 
 
 
