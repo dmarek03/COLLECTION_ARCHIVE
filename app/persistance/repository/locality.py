d178d8bca03154d89397be71cca4e416cd1ce582
dmarek03 <domarek@student.agh.edu.pl>
2025-02-14 10:40:08 +0100

    Added methods to fetch all locality name

--- app/persistance/repository/locality.py
+++ app/persistance/repository/locality.py
@@ -10,20 +10,15 @@ class LocalityRepository(CrudRepository):
         super().__init__(connection_pool, Locality)
         self._create_table()
 
-
-
     def _create_table(self):
         try:
-            create_locality_table = '''
+            create_locality_table = """
              create table if not exists localities(
                     id integer primary key auto_increment,
-                    name varchar(50) not null,
-                    location_id integer,
-                    unique (name),
-                    foreign key (location_id) references locations(id) on delete cascade on update cascade 
-                )'''
-
-
+                    name varchar(50) not null, 
+                    unique (name)
+                   
+                )"""
 
             connection = self.connection_pool.get_connection()
             if connection.is_connected():
@@ -38,6 +33,22 @@ class LocalityRepository(CrudRepository):
                 cursor.close()
                 connection.close()
 
+    def get_all_locality_name(self, descending: bool) -> list[str]:
 
+        try:
+            sql = f" select name from localities order by name"
+            sql += " desc" if descending else ""
 
+            connection = self.connection_pool.get_connection()
+            if connection.is_connected():
+                cursor = connection.cursor()
+                cursor.execute(sql)
 
+                return [l_name[0] for l_name in cursor.fetchall()]
+        except Error as err:
+            logging.error(err)
+            connection.rollback()
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
