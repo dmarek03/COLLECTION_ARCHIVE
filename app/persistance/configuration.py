929fe521842bc636387a68a8c5dc4169d9266b07
dmarek03 <domarek@student.agh.edu.pl>
2025-02-14 10:44:48 +0100

    Added triggers initialization

--- app/persistance/configuration.py
+++ app/persistance/configuration.py
@@ -1,15 +1,16 @@
-from app.db.connection import connection_pool
+from app.db.connection import connection_pool, initialize_triggers
 from .repository.dating import DatingRepository
 from .repository.finder import FinderRepository
-from .repository.location import LocationRepository
 from .repository.locality import LocalityRepository
+from .repository.location import LocationRepository
 from .repository.material import MaterialRepository
 from .repository.founded_items import FoundedItemsRepository
 
-
-dating_repository =  DatingRepository(connection_pool=connection_pool)
+dating_repository = DatingRepository(connection_pool=connection_pool)
 finder_repository = FinderRepository(connection_pool=connection_pool)
-location_repository = LocationRepository(connection_pool=connection_pool)
 locality_repository = LocalityRepository(connection_pool=connection_pool)
+location_repository = LocationRepository(connection_pool=connection_pool)
 material_repository = MaterialRepository(connection_pool=connection_pool)
 founded_items_repository = FoundedItemsRepository(connection_pool=connection_pool)
+if not (x := initialize_triggers(connection_pool=connection_pool)):
+    initialize_triggers(connection_pool=connection_pool)
