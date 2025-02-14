cc36b221d0b47180250b6e7b0ed901b32badbfda
dmarek03 <domarek@student.agh.edu.pl>
2025-02-14 10:45:47 +0100

    Added methods fetching all finder name

--- app/persistance/repository/finder.py
+++ app/persistance/repository/finder.py
@@ -2,6 +2,8 @@ from .generic.repository import CrudRepository
 from app.persistance.model import Finder
 from mysql.connector.pooling import MySQLConnectionPool, Error
 import logging
+
+
 class FinderRepository(CrudRepository):
 
     def __init__(self, connection_pool: MySQLConnectionPool):
@@ -10,20 +12,39 @@ class FinderRepository(CrudRepository):
 
     def _create_table(self):
         try:
-            create_finder_table = '''
+            create_finder_table = """
                 create table if not exists finders(
                     id integer primary key auto_increment,
-                    name varchar(50) not null,
+                    name varchar(50) not null ,
                     unique (name)
                 )
 
-            '''
+            """
 
             connection = self.connection_pool.get_connection()
             if connection.is_connected():
                 cursor = connection.cursor()
                 cursor.execute(create_finder_table)
                 connection.commit()
+        except Error as err:
+            logging.error(err)
+            connection.rollback()
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    def get_all_finder_name(self, descending: bool) -> list[str]:
+
+        try:
+            sql = f""" select name from finders order by name"""
+            sql += " desc" if descending else ""
+            connection = self.connection_pool.get_connection()
+            if connection.is_connected():
+                cursor = connection.cursor()
+                cursor.execute(sql)
+                return [f_name[0] for f_name in cursor.fetchall()]
+
         except Error as err:
             logging.error(err)
             connection.rollback()
