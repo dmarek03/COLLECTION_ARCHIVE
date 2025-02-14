ba1d1c6ed87e83e00bcd9ecce27d865ca8e2eb4b
dmarek03 <domarek@student.agh.edu.pl>
2025-02-14 11:10:59 +0100

    Added button style

---
+++ app/utilities/button_style.py
@@ -0,0 +1,18 @@
+main_button_style = """
+                    QPushButton {
+                        background-color: gold;
+                        color: black;
+                        font-size: 16px;
+                        font-weight: bold;
+                        border-radius: 10px;
+                        padding: 5px;
+                        border: 2px solid #b8860b; 
+                        box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+                    }
+                    QPushButton:hover {
+                        background-color: #ffd700;
+                    }
+                    QPushButton:pressed {
+                        background-color: #daa520; 
+                    }
+                """
