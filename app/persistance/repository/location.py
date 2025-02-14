f1f9995428d1a40e7f662500d2a2026478bac93b
dmarek03 <domarek@student.agh.edu.pl>
2025-02-14 10:41:41 +0100

    Added methods fetching all locations and its coordinates

--- app/persistance/repository/location.py
+++ app/persistance/repository/location.py
@@ -12,18 +12,20 @@ class LocationRepository(CrudRepository):
 
     def _create_table(self):
         try:
-            create_location_table = '''
+            create_location_table = """
                 create table if not exists locations(
                     id integer primary key auto_increment,
-                    name varchar(50) not null,
+                    name varchar(50) default null,
                     latitude double default null,
                     longitude double default null,
                     latitude_direction varchar(3) default null, 
                     longitude_direction varchar(3) default null,
-                    unique(name,latitude,longitude,latitude_direction, longitude_direction)
+                    locality_id integer,
+                    unique(name,latitude,longitude,latitude_direction, longitude_direction,locality_id),
+                    foreign key (locality_id) references localities(id) on delete cascade on update cascade
                 )
 
-            '''
+            """
 
             connection = self.connection_pool.get_connection()
             if connection.is_connected():
@@ -38,4 +40,45 @@ class LocationRepository(CrudRepository):
                 cursor.close()
                 connection.close()
 
+    def get_all_location_name(self, descending: bool) -> list[str]:
 
+        try:
+            sql = f" select name from locations order by name"
+            sql += " desc" if descending else ""
+
+            connection = self.connection_pool.get_connection()
+            if connection.is_connected():
+                cursor = connection.cursor()
+                cursor.execute(sql)
+
+                return [ll_name[0] for ll_name in cursor.fetchall()]
+        except Error as err:
+            logging.error(err)
+            connection.rollback()
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    # TODO think about creating in model coordinates class to represent location cords
+    def get_all_location_coordinates(self, descending: bool) -> list[tuple[str, str]]:
+        try:
+            sql = f" select  latitude, longitude, latitude_direction, longitude_direction from locations order by name"
+            sql += " desc" if descending else ""
+
+            connection = self.connection_pool.get_connection()
+            if connection.is_connected():
+                cursor = connection.cursor()
+                cursor.execute(sql)
+
+                return [
+                    (str(lat) + "°" + lat_d, str(lon) + "°" + lon_d)
+                    for lat, lon, lat_d, lon_d in cursor.fetchall()
+                ]
+        except Error as err:
+            logging.error(err)
+            connection.rollback()
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
