ea038462597a0e11c4043f2f33a72baa6c613199
dmarek03 <domarek@student.agh.edu.pl>
2025-02-20 21:36:04 +0100

    Added item attributes mapping to db column name

---
+++ app/utilities/column_names_dict.py
@@ -0,0 +1,20 @@
+
+item_attributes_to_db_column_mapping = {
+    'Name': 'f.name',
+    'Description': 'f.description',
+    'First image': 'f.first_image_data',
+    'Second image': 'f.second_image_data',
+    'Quantity': 'f.quantity',
+    'Finding date': 'f.finding_date',
+    'Addition date': 'f.addition_date',
+    'Finder name': 'fd.name',
+    'Locality name': 'l.name',
+    'Location name': 'll.name',
+    'Latitude': 'll.latitude',
+    'Longitude': 'll.longitude',
+    'Latitude direction': 'll.latitude_direction',
+    'Longitude direction': 'll.longitude_direction',
+    'Material name': 'm.name',
+    'Epoch name': 'd.name',
+    'Year': 'd.year'
+}
