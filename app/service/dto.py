f0ab48a72258fee2c885a8da618014c28f969e18
dmarek03 <domarek@student.agh.edu.pl>
2025-02-20 21:26:53 +0100

    Updated dto

--- app/service/dto.py
+++ app/service/dto.py
@@ -1,13 +1,15 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, date
 
 
 @dataclass
 class CreateFinalItemDto:
     name: str | None = None
     description: str | None = None
-    image_data: bytes | None = None
+    first_image_data: bytes | None = None
+    second_image_data: bytes | None = None
     quantity: int | None = 0
+    finding_date: date | None = None
     addition_date: datetime = datetime.now()
     finder_name: str | None = None
     locality_name: str | None = None
@@ -22,17 +24,19 @@ class CreateFinalItemDto:
 
     def __repr__(self):
         return f"""
-               Name: {self.name},
-               Description: {self.description},
-               Image bytes: {len(self.image_data)},
-               Quantity: {self.quantity},
-               Date of addition: {self.addition_date},
-               Name of finder: {self.finder_name},
-               Name of locality: {self.locality_name},
-               Name of location: {self.location_name if self.location_name else 'Indefinite'},
-               Coordinates: {self.latitude}°{self.latitude_direction} , {self.longitude}°{self.longitude_direction},
-               Name of material: {self.material_name},
-               Name of the epoch: {self.epoch_name},
+               Name: {self.name}
+               Description: {self.description}
+               First image bytes: {len(self.first_image_data)}
+               Second image bytes: {len(self.second_image_data)}
+               Quantity: {self.quantity}
+               Date of addition: {self.addition_date}
+               Date of finding: {self.finding_date}
+               Name of finder: {self.finder_name}
+               Name of locality: {self.locality_name}
+               Name of location: {self.location_name if self.location_name else 'Indefinite'}
+               Coordinates: {self.latitude}°{self.latitude_direction} , {self.longitude}°{self.longitude_direction}
+               Name of material: {self.material_name}
+               Name of the epoch: {self.epoch_name}
                Year: {self.year if self.year else 'Indefinite'}
 
            """
