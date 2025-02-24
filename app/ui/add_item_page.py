b020b027e7fbfad56b355081f39f7ef12be9cc09
dmarek03 <domarek@student.agh.edu.pl>
2025-02-24 22:17:45 +0100

    Managed updating item

--- app/ui/add_item_page.py
+++ app/ui/add_item_page.py
@@ -46,17 +46,19 @@ class AddItemPage(QWidget):
         self.epoch_name = QLineEdit(self)
         self.year = QLineEdit(self)
         self.stacked_widget = stacked_widget
+        self.layout = QGridLayout(self)
+        self.save_button = QPushButton("Save Item")
+        self.save_updated_button = QPushButton('Save updated item')
         self.init_ui()
 
     def init_ui(self):
-        layout = QGridLayout(self)
-        layout.setSpacing(10)
+        self.layout.setSpacing(10)
 
         item_name_label = QLabel("Item Name:")
         description_name_label = QLabel("Description:")
         item_first_image_label = QLabel("First Image:")
         item_second_image_label = QLabel("Second Image:")
-        finding_date_label = QLabel('Date of finding:')
+        finding_date_label = QLabel("Date of finding:")
         quantity_label = QLabel("Quantity:")
         finder_name_label = QLabel("Name of finder:")
         locality_name_label = QLabel("Name of locality:")
@@ -111,63 +113,60 @@ class AddItemPage(QWidget):
         """
         )
 
-        save_button = QPushButton("Save Item")
-        save_button.setStyleSheet(main_button_style)
-        save_button.clicked.connect(self.save_item)
+        self.create_save_button()
 
         back_button = QPushButton("Back to Main Page")
         back_button.setStyleSheet(main_button_style)
         back_button.clicked.connect(self.go_to_start_window)
 
-        layout.addWidget(item_name_label, 0, 0)
-        layout.addWidget(self.item_name, 0, 1)
-        layout.addWidget(description_name_label, 1, 0)
-        layout.addWidget(self.description, 1, 1, 1, 2)
-        layout.addWidget(item_first_image_label, 1, 4)
-        layout.addWidget(self.first_image, 1, 5)
-        layout.addWidget(item_second_image_label, 1, 6)
-        layout.addWidget(self.second_image, 1, 7)
-        layout.addWidget(finding_date_label, 2, 0)
-        layout.addWidget(self.finding_date, 2, 1)
-        layout.addWidget(quantity_label, 3, 0)
-        layout.addWidget(self.quantity, 3, 1)
-        layout.addWidget(finder_name_label, 4, 0)
-        layout.addWidget(self.finder_name, 4, 1)
-        layout.addWidget(locality_name_label, 5, 0)
-        layout.addWidget(self.locality_name, 5, 1)
-        layout.addWidget(location_name_label, 5, 2)
-        layout.addWidget(self.location_name, 5, 3)
-        layout.addWidget(latitude_label, 6, 0)
-        layout.addWidget(self.latitude, 6, 1)
-        layout.addWidget(latitude_direction_label, 6, 2)
-        layout.addWidget(self.latitude_direction, 6, 3)
-        layout.addWidget(longitude_label, 6, 4)
-        layout.addWidget(self.longitude, 6, 5)
-        layout.addWidget(longitude_direction_label, 6, 6)
-        layout.addWidget(self.longitude_direction, 6, 7)
-        layout.addWidget(material_label, 7, 0)
-        layout.addWidget(self.material_name, 7, 1)
-        layout.addWidget(epoch_label, 8, 0)
-        layout.addWidget(self.epoch_name, 8, 1)
-        layout.addWidget(year_label, 8, 2)
-        layout.addWidget(self.year, 8, 3)
+        self.layout.addWidget(item_name_label, 0, 0)
+        self.layout.addWidget(self.item_name, 0, 1)
+        self.layout.addWidget(description_name_label, 1, 0)
+        self.layout.addWidget(self.description, 1, 1, 1, 2)
+        self.layout.addWidget(item_first_image_label, 1, 4)
+        self.layout.addWidget(self.first_image, 1, 5)
+        self.layout.addWidget(item_second_image_label, 1, 6)
+        self.layout.addWidget(self.second_image, 1, 7)
+        self.layout.addWidget(finding_date_label, 2, 0)
+        self.layout.addWidget(self.finding_date, 2, 1)
+        self.layout.addWidget(quantity_label, 3, 0)
+        self.layout.addWidget(self.quantity, 3, 1)
+        self.layout.addWidget(finder_name_label, 4, 0)
+        self.layout.addWidget(self.finder_name, 4, 1)
+        self.layout.addWidget(locality_name_label, 5, 0)
+        self.layout.addWidget(self.locality_name, 5, 1)
+        self.layout.addWidget(location_name_label, 5, 2)
+        self.layout.addWidget(self.location_name, 5, 3)
+        self.layout.addWidget(latitude_label, 6, 0)
+        self.layout.addWidget(self.latitude, 6, 1)
+        self.layout.addWidget(latitude_direction_label, 6, 2)
+        self.layout.addWidget(self.latitude_direction, 6, 3)
+        self.layout.addWidget(longitude_label, 6, 4)
+        self.layout.addWidget(self.longitude, 6, 5)
+        self.layout.addWidget(longitude_direction_label, 6, 6)
+        self.layout.addWidget(self.longitude_direction, 6, 7)
+        self.layout.addWidget(material_label, 7, 0)
+        self.layout.addWidget(self.material_name, 7, 1)
+        self.layout.addWidget(epoch_label, 8, 0)
+        self.layout.addWidget(self.epoch_name, 8, 1)
+        self.layout.addWidget(year_label, 8, 2)
+        self.layout.addWidget(self.year, 8, 3)
 
-        layout.addWidget(save_button, 9, 0, 1, 8, Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(back_button, 10, 0, 1, 8, Qt.AlignmentFlag.AlignCenter)
+        self.layout.addWidget(back_button, 10, 0, 1, 8, Qt.AlignmentFlag.AlignCenter)
 
-        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layout.setColumnStretch(5, 1)
-        layout.setColumnStretch(7, 1)
-        layout.setColumnStretch(1, 1)
-        layout.setColumnStretch(3, 1)
+        self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.layout.setColumnStretch(5, 1)
+        self.layout.setColumnStretch(7, 1)
+        self.layout.setColumnStretch(1, 1)
+        self.layout.setColumnStretch(3, 1)
 
-        layout.setRowMinimumHeight(0, 40)
-        layout.setColumnMinimumWidth(1, 150)
-        layout.setColumnMinimumWidth(3, 150)
-        layout.setColumnMinimumWidth(5, 200)
-        layout.setColumnMinimumWidth(7, 200)
-        layout.setSpacing(10)
-        self.setLayout(layout)
+        self.layout.setRowMinimumHeight(0, 40)
+        self.layout.setColumnMinimumWidth(1, 150)
+        self.layout.setColumnMinimumWidth(3, 150)
+        self.layout.setColumnMinimumWidth(5, 200)
+        self.layout.setColumnMinimumWidth(7, 200)
+        self.layout.setSpacing(10)
+        self.setLayout(self.layout)
 
     @staticmethod
     def reset_style(widget):
@@ -175,7 +174,7 @@ class AddItemPage(QWidget):
         widget.setStyleSheet("")
         widget.setPlaceholderText("")
 
-    def reset_fields(self):
+    def reset_fields(self) -> None:
         self.item_name.clear()
         self.description.clear()
         self.first_image.clear()
@@ -193,9 +192,27 @@ class AddItemPage(QWidget):
         self.epoch_name.clear()
         self.year.clear()
 
+    def set_fields(self, item: CreateFinalItemDto) -> None:
+        self.item_name.setText(item.name)
+        self.description.setText(item.description)
+        self.first_image.clear()
+        self.second_image.clear()
+        self.finding_date.setDate(item.finding_date)
+        self.quantity.setText(str(item.quantity))
+        self.finder_name.setText(item.finder_name)
+        self.locality_name.setText(item.locality_name)
+        self.location_name.setText(item.location_name)
+        self.latitude.setText(str(item.latitude))
+        self.longitude.setText(str(item.longitude))
+        self.latitude_direction.setCurrentIndex(0)
+        self.longitude_direction.setCurrentIndex(0)
+        self.material_name.setText(item.material_name)
+        self.epoch_name.setText(item.epoch_name)
+        self.year.setText(str(item.year))
+
     def setup_signals(self):
 
-        self.item_name.textChanged.connect(lambda x: self.reset_style(self.item_name))
+        self.item_name.textChanged.connect(lambda: self.reset_style(self.item_name))
         self.description.textChanged.connect(lambda: self.reset_style(self.description))
         self.quantity.textChanged.connect(lambda: self.reset_style(self.quantity))
         self.finder_name.textChanged.connect(lambda: self.reset_style(self.finder_name))
@@ -227,7 +244,9 @@ class AddItemPage(QWidget):
             mark_invalid(self.description, "Description is required")
 
         if not self.first_image.get_image() or not self.second_image.get_image():
+
             errors.append("Images are required")
+            QMessageBox.warning(self, 'Lack of item images', 'Images are required')
 
         if not self.quantity.text():
             mark_invalid(self.quantity, "Quantity is required")
@@ -281,37 +300,90 @@ class AddItemPage(QWidget):
 
         return True
 
-    def save_item(self):
+    def create_item_from_data(self) -> CreateFinalItemDto:
+        return CreateFinalItemDto(
+            name=self.item_name.text().strip(),
+            description=self.description.toPlainText(),
+            first_image_data=self.first_image.convert_to_bytes(),
+            second_image_data=self.second_image.convert_to_bytes(),
+            finding_date=self.finding_date.date().toString("yyyy-MM-dd"),
+            quantity=int(self.quantity.text()),
+            finder_name=self.finder_name.text(),
+            locality_name=self.locality_name.text(),
+            location_name=self.location_name.text(),
+            latitude=float(self.latitude.text()),
+            longitude=float(self.longitude.text()),
+            latitude_direction=self.latitude_direction.currentText(),
+            longitude_direction=self.longitude_direction.currentText(),
+            material_name=self.material_name.text(),
+            epoch_name=self.epoch_name.text(),
+            year=int(self.year.text()) if self.year.text() else None,
+        )
+
+    def create_save_button(self) -> None:
+        self.save_button = QPushButton('Save')
+        self.save_button.setStyleSheet(main_button_style)
+        self.save_button.clicked.connect(self.save_item)
+        self.layout.addWidget(self.save_button, 9, 0, 1, 8, Qt.AlignmentFlag.AlignCenter)
+
+    def create_save_updated_button(self, item: CreateFinalItemDto) -> None:
+        self.save_updated_button = QPushButton('Save updated item')
+        self.save_updated_button.setStyleSheet(main_button_style)
+        self.save_updated_button.clicked.connect(lambda _, w=item: self.update_item(w))
+        self.layout.addWidget(self.save_updated_button, 9, 0, 1, 8, Qt.AlignmentFlag.AlignCenter)
+
+    def go_to_edition_page(self, item: CreateFinalItemDto) -> None:
+
+        self.layout.removeWidget(self.save_button)
+        self.save_button.deleteLater()
+
+        self.create_save_updated_button(item)
+
+        self.set_fields(item)
+
+    def update_item(self, item: CreateFinalItemDto) -> None:
 
         if self.check_validation():
 
-            item = CreateFinalItemDto(
-                name=self.item_name.text(),
-                description=self.description.toPlainText(),
-                first_image_data=self.first_image.convert_to_bytes(),
-                second_image_data=self.second_image.convert_to_bytes(),
-                finding_date=self.finding_date.date().toString("yyyy-MM-dd"),
-                quantity=int(self.quantity.text()),
-                finder_name=self.finder_name.text(),
-                locality_name=self.locality_name.text(),
-                location_name=self.location_name.text(),
-                latitude=float(self.latitude.text()),
-                longitude=float(self.longitude.text()),
-                latitude_direction=self.latitude_direction.currentText(),
-                longitude_direction=self.longitude_direction.currentText(),
-                material_name=self.material_name.text(),
-                epoch_name=self.epoch_name.text(),
-                year=int(self.year.text()) if self.year.text() else None,
-            )
-
+            updated_item = self.create_item_from_data()
             try:
-                self.item_service.add_final_item(create_final_item_dto=item)
+                self.item_service.update_final_item(old_item=item, updated_item=updated_item)
 
             except Error as err:
                 QMessageBox.warning(self, 'Submission incorrect', err.msg)
 
             finally:
-                QMessageBox.information(self, "Submission correct", "Item saved correctly")
+                QMessageBox.information(
+                    self, "Submission correct", "Item updated correctly"
+                )
+                collection_page = self.stacked_widget.widget(1)
+                collection_page.create_single_pages()
+                collection_page.init_filter_bar()
+
+                self.reset_fields()
+                self.layout.removeWidget(self.save_updated_button)
+                self.save_updated_button.deleteLater()
+                self.create_save_button()
+
+    def save_item(self):
+
+        if self.check_validation():
+
+            item = self.create_item_from_data()
+
+            try:
+                self.item_service.add_final_item(create_final_item_dto=item)
+
+            except Error as err:
+                QMessageBox.warning(self, "Submission incorrect", err.msg)
+
+            finally:
+                QMessageBox.information(
+                    self, "Submission correct", "Item saved correctly"
+                )
+                collection_page = self.stacked_widget.widget(1)
+                collection_page.create_single_pages()
+                collection_page.init_filter_bar()
                 self.reset_fields()
 
     def go_to_start_window(self):
